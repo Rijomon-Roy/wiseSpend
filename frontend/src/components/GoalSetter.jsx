416a03eef4bb02e83963cdebf2e24d990dafb000
Rijo Roy <rijoroykallattu@gmail.com>
2026-02-03 23:59:37 +0530

    goal set error fixing

--- frontend/src/components/GoalSetter.jsx
+++ frontend/src/components/GoalSetter.jsx
@@ -1,12 +1,22 @@
 function GoalSetter({ goal, setGoal }) {
+  const handleChange = (e) => {
+    const value = e.target.value;
+
+    // remove leading zeros + convert to number
+    const cleanNumber = Number(value);
+
+    setGoal(cleanNumber);
+  };
+
   return (
     <div className="flex items-center gap-3">
       <label className="font-medium">Monthly Goal ₹</label>
 
       <input
         type="number"
-        value={goal}
-        onChange={(e) => setGoal(Number(e.target.value))}
+        value={goal === 0 ? "" : goal} // ⭐ important trick
+        onChange={handleChange}
+        placeholder="Enter goal"
         className="border rounded px-3 py-2 w-32"
       />
     </div>
