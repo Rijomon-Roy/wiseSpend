bd353160d6c8ae651472ad36fe06d088f7b4afc1
Rijo Roy <102303624+Rijomon-Roy@users.noreply.github.com>
2026-06-20 23:13:49 +0530

    Update registration API endpoint to include '/api/auth'

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = "https://wisespend-backend.onrender.com";
 
 export const registerUser = (data) =>
-  axios.post(`${API}/register`, data);
+  axios.post(`${API}/api/auth/register`, data);
 
 export const loginUser = (data) =>
   axios.post(`${API}/login`, data);
