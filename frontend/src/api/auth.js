2f4168e58106d74ad4dabe8d68027e863bf95dfc
Rijo Roy <102303624+Rijomon-Roy@users.noreply.github.com>
2026-06-20 23:46:46 +0530

    Update login API endpoint to include '/api/auth'

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -6,4 +6,4 @@ export const registerUser = (data) =>
   axios.post(`${API}/api/auth/register`, data);
 
 export const loginUser = (data) =>
-  axios.post(`${API}/login`, data);
+  axios.post(`${API}/api/auth/login`, data);
