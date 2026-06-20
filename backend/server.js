fe884c4cc2301485924ee12b5215ae6584e7ac4a
Rijomon Roy <rijoroykallattu@gmail.com>
2026-06-20 20:17:17 +0530

    Fix Render deployment

--- backend/server.js
+++ backend/server.js
@@ -20,4 +20,8 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
